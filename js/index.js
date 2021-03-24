c2091c19354ea2dae1091168130214c739a16206
chrislemus <icrislemus@gmail.com>
2021-03-24 10:58:34 -0400

    keyboard styles

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
   alphabet.forEach(letter => {
     const li =  document.createElement('li');
-    li.innerHTML = letter
+    li.innerHTML = letter.toUpperCase()
     li.classList.add("letter")
     keyboardWrapper.appendChild(li)
   })
