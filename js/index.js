980f0c5615d32fcf21e9945f798e7d6627ae8b1c
chrislemus <icrislemus@gmail.com>
2021-03-25 11:40:35 -0400

    added category fetch and display category dropdown once fetch request completes

--- js/index.js
+++ js/index.js
@@ -7,6 +7,30 @@ document.addEventListener('DOMContentLoaded', () => {
     li.classList.add("letter")
     keyboardWrapper.appendChild(li)
   })
-  
+
+  fetch('http://localhost:3000/categories')
+  .then(res => res.json())
+  .then(categories => addCategorySelect(categories));
 });
-window.addEventListener('')
+
+
+function addCategorySelect(categories) {
+  const categoriesWrapper = document.querySelector('.categories-wrapper')
+  const dropdown = document.querySelector('#categories-dropdown')
+  const loadingBar = document.querySelector('.loading-bar')
+
+  loadingBar.classList.add('is-invisible')
+  categoriesWrapper.classList.remove('is-invisible')
+  
+  const categoryOptions = categories.map(({id, name}) => `<option value=${id}>${name}</option>'`)
+  dropdown.innerHTML += categoryOptions
+
+}
+
+function getPhraseByCategory() {
+  const categoryId = document.querySelector('#categories-dropdown').value
+  console.log(dropdown.value)
+}
+
+
+// window.addEventListener('')
